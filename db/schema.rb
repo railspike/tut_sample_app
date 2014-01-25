019f34ac37fde394aa8e87590d233d0ae76bf7ca
railspike <spcantwell@gmail.com>
2014-01-24 21:33:53 -0800

    Through 6.2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140120021202) do
+ActiveRecord::Schema.define(version: 20140125052011) do
 
   create_table "comments", force: true do |t|
     t.string   "body"
@@ -38,5 +38,7 @@ ActiveRecord::Schema.define(version: 20140120021202) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
